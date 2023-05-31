0062d409ce54452d4bf0d7bf6511b4b1d791ba15
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-05-31 18:36:35 +0200

    Add bonus TrainTicketCalc exercise - ticket price with 2 decimal places - refactoring discount print

--- src/org/lessons/java/TrainTicketCalc.java
+++ src/org/lessons/java/TrainTicketCalc.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TrainTicketCalc {
@@ -12,18 +13,24 @@ public class TrainTicketCalc {
         System.out.print("Inserisci la tua età: ");
         int userAge = inputUser.nextInt();
 
+        // Define variables values
         double pricePerKm = 0.21;
         double ticketPrice = distance * pricePerKm;
+        double discount = 0.0;
         double discount20 = 0.2;
         double discount40 = 0.4;
 
         // Define discount conditions
-        if (userAge <= 18) {
-            ticketPrice -= ticketPrice * discount20;
-            System.out.println("Il costo del tuo biglietto scontato del " + (discount20 * 100) + "% è: "  + ticketPrice + "€");
-        } else if (userAge > 65) {
-            ticketPrice -= ticketPrice * discount40;
-            System.out.println("Il costo del tuo biglietto scontato del " + (discount40 * 100) + "% è: " + ticketPrice + "€");
-        } else System.out.println("Il costo del tuo biglietto è: " + ticketPrice + "€");
+        if (userAge <= 18) discount = discount20;
+        else if (userAge > 65) discount = discount40;
+
+        // Calculate ticket price
+        ticketPrice -= ticketPrice * discount;
+
+        DecimalFormat decimalFormat = new DecimalFormat(".00");
+        String formattedTicketPrice = decimalFormat.format(ticketPrice);
+
+        if (discount > 0) System.out.println("Il costo del tuo biglietto scontato del " + ((int) (discount * 100)) + "% è: "  + formattedTicketPrice + "€");
+        else System.out.println("Il costo del tuo biglietto intero è: " + formattedTicketPrice + "€");
     }
 }
